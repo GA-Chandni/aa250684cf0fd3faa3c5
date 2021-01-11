6b1b9fd2be3726c89a69af79a8d41c6e06f2a34d
GA-Chandni <chandni.yadav@green-apex.com>
2021-01-11 15:57:54 +0530

    initial commit

---
+++ src/components/originalRowDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OriginalRowDetails =(props)=> {
+    const details= props.location.data;
+    return (
+        <div>
+            {JSON.stringify(details)}
+        </div>
+    )
+}
+
+export default OriginalRowDetails
